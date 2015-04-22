f2e8a4c87a00c9a9ae4d397d911a7129b6dc8c72
ugur-mehmet <ugur_mehmet@yahoo.com>
2015-04-22 10:20:21 -0400

    Updated

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,32 @@
+if (Meteor.users.find().fetch().length === 0) {
+
+    console.log('Creating users: ');
+
+    var users = [
+        {name:"Normal User",username:"alarm",roles:[]},
+        // {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
+        // {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']},
+        {name:"Admin User",username:"admin",roles:['admin']}
+      ];
+
+    _.each(users, function (userData) {
+      var id,user;
+
+      console.log(userData);
+
+      id = Accounts.createUser({
+        username: userData.username,
+        password: "alarm",
+        profile: { name: userData.name }
+      });
+
+      // email verification
+      // Meteor.users.update({_id: id}, {$set:{'emails.0.verified': true}});
+
+      Roles.addUsersToRoles(id, userData.roles);
+
+    });
+  }
 
 if (Alarms.find().count()===0){
   Alarms.insert({
